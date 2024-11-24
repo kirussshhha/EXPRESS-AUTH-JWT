bb2bfce92875654ace331899849c27410f144ae2
kirussshhha <kirill2006may@gmail.com>
2024-11-24 22:46:39 +0300

    AUT-8: Add validation

--- src/delivery/userRoutes.js
+++ src/delivery/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { signIn, signUp } from "./userController.js";
+import validateRequest from "../middlewares/validation/validateRequest.js";
+import orderSchema from "../middlewares/validation/schema/userSchema.js";
 
 const router = express.Router();
 
-router.post("/signUp", signUp);
+router.post("/signUp",validateRequest(orderSchema), signUp);
 router.get("/signIn", signIn);
 
 export default router;
